f078dcdd6352ea9fb014c9856adce4639afdd109
workspace014-admin <oscar.osorio1884@gmail.com>
2025-04-19 11:00:21 -0500

    Changes SafeAreaProvider

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const icon = require("../assets/icon.png");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={icon}
-        style={{ width: 100, height: 100 }}
-        resizeMode="contain"
-      />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Image
+          source={icon}
+          style={{ width: 100, height: 100 }}
+          resizeMode="contain"
+        />
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
